9d9051882159bfd7168baf3bf8fbf5cb06187161
ChouChihYu <82814699+ChouChihYu@users.noreply.github.com>
2021-04-20 15:39:08 +0800

    update

--- Build/RecycleGame.framework.js
+++ Build/RecycleGame.framework.js
@@ -1296,7 +1296,7 @@ function _emscripten_asm_const_ii(code, a0) {
  return ASM_CONSTS[code](a0);
 }
 STATIC_BASE = GLOBAL_BASE;
-STATICTOP = STATIC_BASE + 2331792;
+STATICTOP = STATIC_BASE + 2331856;
 __ATINIT__.push({
  func: (function() {
   __GLOBAL__sub_I_AIScriptingClasses_cpp();
@@ -3226,7 +3226,7 @@ __ATINIT__.push({
   ___emscripten_environ_constructor();
  })
 });
-var STATIC_BUMP = 2331792;
+var STATIC_BUMP = 2331856;
 Module["STATIC_BASE"] = STATIC_BASE;
 Module["STATIC_BUMP"] = STATIC_BUMP;
 var tempDoublePtr = STATICTOP;
